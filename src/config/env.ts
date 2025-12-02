1752f9b171fc06bfb38aabf4e5a4fe09459ce6fb
THARUNJP <tharunroshini71@gmail.com>
2025-12-02 13:28:55 +0530

    prisma setup added

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,19 +1,17 @@
 import z from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
   PORT: z.string().default("8000"), // read as string from env
   DATABASE_URL: z.string(),
   SHADOW_DATABASE_URL: z.string(),
-  ACCESS_TOKEN_SECRET: z.string(),
-  REFRESH_TOKEN_SECRET: z.string(),
-  FORGET_PASSWORD_SECRET:z.string()
 });
 
 // 2. Parse and validate
 
 const env = envSchema.parse(process.env);
-
 
 // 3. Export centralized config
 
@@ -25,10 +23,4 @@ export const appEnv = {
     main: env.DATABASE_URL,
     shadow: env.SHADOW_DATABASE_URL,
   },
-  jwt: {
-    accessSecret: env.ACCESS_TOKEN_SECRET,
-    refreshSecret: env.REFRESH_TOKEN_SECRET,
-    forgetPassSecret:env.FORGET_PASSWORD_SECRET,
-  },
 };
-
