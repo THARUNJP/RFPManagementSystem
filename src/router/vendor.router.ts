f74d41f18fcc88b70586faf0ee8618c19cc75372
THARUNJP <tharunroshini71@gmail.com>
2025-12-02 23:30:16 +0530

    router addded

--- src/router/vendor.router.ts
+++ src/router/vendor.router.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { validators } from "../validators/index.validator";
-import { createVendor, listVendors, updateVendor } from "../controller/vendor.controller";
+import { createVendor, deleteVendor, listVendors, updateVendor } from "../controller/vendor.controller";
 
 const router = Router();
 
 router.post("/",validators.createVendor,createVendor);
 router.get("/",listVendors);
 router.patch("/:vendor_id",validators.updateVendor,updateVendor);
-router.delete("/:vendor_id", () => {});
+router.delete("/:vendor_id",deleteVendor);
 
 export default router;
