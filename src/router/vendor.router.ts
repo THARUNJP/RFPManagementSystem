99089d5d9eb98b0d7459343b9318dc8e5c414aca
THARUNJP <tharunroshini71@gmail.com>
2025-12-02 23:06:30 +0530

    c

--- src/router/vendor.router.ts
+++ src/router/vendor.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { validators } from "../validators/index.validator";
+import { createVendor } from "../controller/vendor.controller";
 
 const router = Router();
 
-router.post("/", () => {});
+router.post("/",validators.createVendor,createVendor);
 router.get("/", () => {});
 router.patch("/:vendor_id", () => {});
 router.delete("/:vendor_id", () => {});
