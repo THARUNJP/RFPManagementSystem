fcda98618ee65b58db598fac4726a917b0585281
THARUNJP <tharunroshini71@gmail.com>
2025-12-02 23:36:45 +0530

    router for vendor added, vendor compelted:needs to be tested

--- src/router/vendor.router.ts
+++ src/router/vendor.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { validators } from "../validators/index.validator";
-import { createVendor, deleteVendor, listVendors, updateVendor } from "../controller/vendor.controller";
+import { createVendor, deleteVendor, getVendorById, listVendors, updateVendor } from "../controller/vendor.controller";
 
 const router = Router();
 
 router.post("/",validators.createVendor,createVendor);
 router.get("/",listVendors);
+router.get("/:vendor_id",getVendorById)
 router.patch("/:vendor_id",validators.updateVendor,updateVendor);
 router.delete("/:vendor_id",deleteVendor);
 
