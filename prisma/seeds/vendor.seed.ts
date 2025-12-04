a7d7e0f041b4312e5bd1925dba0c35a5eb22def0
THARUNJP <tharunroshini71@gmail.com>
2025-12-04 15:53:27 +0530

    seeds up file added

--- prisma/seeds/vendor.seed.ts
+++ prisma/seeds/vendor.seed.ts
@@ -3,9 +3,15 @@
 import { prisma } from "../../src/config/prisma";
 
 export async function seedVendors() {
-  const vendors = [
-    { name: "Vendor One", contact_email: "vendor1@example.com", phone: "1234567890" },
-    { name: "Vendor Two", contact_email: "vendor2@example.com", phone: "0987654321" },
+   const vendors = [
+    
+    { name: "Vendor Three", contact_email: "vendor3@example.com", phone: "1112223333" },
+    { name: "Vendor Four", contact_email: "vendor4@example.com", phone: "2223334444" },
+    { name: "Vendor Five", contact_email: "vendor5@example.com", phone: "3334445555" },
+    { name: "Vendor Six", contact_email: "vendor6@example.com", phone: "4445556666" },
+    { name: "Vendor Seven", contact_email: "vendor7@example.com", phone: "5556667777" },
+    { name: "Vendor Eight", contact_email: "vendor8@example.com", phone: "6667778888" },
+    { name: "Vendor Nine", contact_email: "vendor9@example.com", phone: "7778889999" },
   ];
 
   await prisma.vendors.createMany({
