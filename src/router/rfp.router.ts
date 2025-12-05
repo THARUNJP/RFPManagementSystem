dc2bddbcefbd10dad7356fee4f5babce20b6a015
THARUNJP <tharunroshini71@gmail.com>
2025-12-05 14:30:01 +0530

    route fixed

--- src/router/rfp.router.ts
+++ src/router/rfp.router.ts
@@ -11,8 +11,8 @@ router.get("/:rfp_id", getRfpById);
 router.get("/", listRfps);
 router.post("/", validators.createRfp, createRfp);
 router.post("/:rfp_id/send",validators.sendRfp,sendRfp);
-router.get("/rfp/:rfp_id/vendors",getRfpVendors);
-router.get("/rfp/:rfp_id/proposal",getProposals)
+router.get("/:rfp_id/vendors",getRfpVendors);
+router.get("/:rfp_id/proposal",getProposals)
 
 
 
